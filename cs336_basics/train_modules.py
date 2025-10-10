81263d642bd8add2c455e85b789771f856c912e6
Hanchao <2731293166@qq.com>
2025-10-10 14:12:24 +0800

    feat: lr secheule and gradient clip

--- cs336_basics/train_modules.py
+++ cs336_basics/train_modules.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn
 from einops import reduce, einsum, repeat
+from typing import Iterable
+import math
 
 def cross_entropy(logit: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
     vocab_size = logit.shape[-1]
@@ -23,3 +25,26 @@ def cross_entropy(logit: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
 def calculate_perplexity(loss: torch.Tensor) -> torch.Tensor:
     ''' loss[..., i] is the loss of token `i` in the sequence: loss(x_i | x_{1...i-1}) '''
     return torch.exp(torch.sum(loss, dim=-1) / loss.shape[-1]).mean()
+
+def learning_rate_schedule(current_t: int, max_lr: float, min_lr: float, warmup_t: int, cosine_annealing_t: int) -> float:
+    if current_t < warmup_t:
+        return (current_t / warmup_t) * max_lr # increase lr from 0 to max_lr linearly
+    elif current_t <= cosine_annealing_t:
+        # lr decrease from max_lr to min_lr smoothly
+        return min_lr + (1 + math.cos((current_t - warmup_t)/(cosine_annealing_t - warmup_t)*math.pi)) / 2 * (max_lr - min_lr)
+    else:
+        return min_lr # at the end of training, keep small lr for stability
+
+def gradient_clipping(params: Iterable[nn.Parameter], max_norm: float, eps: float = 1e-6):
+    all_grads = [p.grad.data for p in params if p.grad is not None]
+    # why need to concat the gradients of all the parameters and then calculate the norm?
+    # To keep the direction of the gradient vector!
+    # In one backward and step, all the parameters can be seen one single large vector
+    # and the gradient of each parameter consist the gradient vector
+    # if calculate for each gradient, the composed gradient vector will have the different directions
+    total_norm = torch.norm(torch.stack(tuple(g.view(-1) for g in all_grads))) # for l2-norm, flatten the gradient is the same
+    if total_norm >= max_norm:
+        factor = max_norm / (total_norm + eps)
+        for p in params:
+            if p.grad is not None:
+                p.grad.data *= factor
